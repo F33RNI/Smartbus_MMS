7dc397de3f82117f568cb0a497b6860e6ce54e9d
Frey Hertz <39090123+XxOinvizioNxX@users.noreply.github.com>
2021-04-26 19:29:49 -0500

    Update SMARTBUS_MMS.ino

--- SMARTBUS_MMS/SMARTBUS_MMS.ino
+++ SMARTBUS_MMS/SMARTBUS_MMS.ino
@@ -1,14 +1,18 @@
 /*
-*
-* SMARTBUS
-* Main motor Control System
-* __________________
-*
-*  © 2019, Neshumov Pavel Evgenyevich
-*  All Rights Reserved.
-*
-*  No copying allowed.
-*/
+ * Copyright 2019 Smartbus main motor controller
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
 #define IgnitionPin 2 //Ignition relay
